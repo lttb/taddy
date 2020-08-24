71b79b1d7b24cde894205dd3d8611ba9e781ffc0
lttb <kenzhaev.artur@gmail.com>
2020-08-24 20:17:20 +0300

    feat(babel-plugin): fix bindings optimization

--- packages/babel-plugin/src/helpers/index.ts
+++ packages/babel-plugin/src/helpers/index.ts
@@ -3,5 +3,5 @@ export * from './evaluate';
 export * from './findBindings';
 export * from './getObjectPropertyKey';
 export * from './mergeObjectProperties';
-export * from './optimizeBindings';
+export * from './BindingOptimizer';
 export * from './optimizeStaticStyles';
--- packages/babel-plugin/src/tests/typescript.test.ts
+++ packages/babel-plugin/src/tests/typescript.test.ts
@@ -83,7 +83,6 @@ describe('taddy.macro.typescript', () => {
             ),
         ).toMatchInlineSnapshot(`
             "import { css } from \\"@taddy/core\\";
-            import { box, typo } from './data/mixins';
             export default css(\\"_a1f1_dd91 _3e02_3da8 _8ef9_849b _69bc_9bfd_4da4 _9bfd_4da4 _15b0_2efe\\", \\"__376slgo\\");"
         `);
 
