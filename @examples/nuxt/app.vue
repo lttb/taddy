f7b52860800c0dc868bc24b17a94b03e0407174d
lttb <kenzhaev.artur@gmail.com>
2023-03-17 23:05:10 +0000

    feat(examples/nuxt): initial example

---
+++ @examples/nuxt/app.vue
@@ -0,0 +1,14 @@
+<script>
+import {config} from 'taddy';
+
+config({
+    unstable_mapStyles: (result) => ({
+        class: result.className,
+        style: result.style,
+    }),
+});
+</script>
+
+<template>
+    <NuxtPage />
+</template>
---
+++ @examples/nuxt/pages/test.vue
@@ -0,0 +1,13 @@
+<script>
+import {css} from 'taddy';
+
+export default {
+    data: () => ({
+        h1: css({color: 'red'}),
+    }),
+};
+</script>
+
+<template>
+    <h1 v-bind="h1">Hello world!</h1>
+</template>
