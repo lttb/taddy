44f15c422de1ba4cab96b8645ba902f24a77f69d
lttb <kenzhaev.artur@gmail.com>
2023-03-17 18:05:14 +0000

    feat(examples/nextjs): compile styles into one file

--- @examples/nextjs/next.config.js
+++ @examples/nextjs/next.config.js
@@ -7,6 +7,24 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
+
+    webpack(config, {dev}) {
+        if (!dev) {
+            // @see https://github.com/webpack-contrib/mini-css-extract-plugin#extracting-all-css-in-a-single-file
+            config.optimization.splitChunks.cacheGroups = {
+                ...config.optimization.splitChunks.cacheGroups,
+                styles: {
+                    name: 'styles',
+                    type: 'css/mini-extract',
+                    test: /\.taddy\.css$/,
+                    chunks: 'all',
+                    enforce: true,
+                },
+            };
+        }
+
+        return config;
+    },
 };
 
 module.exports = nextConfig;
