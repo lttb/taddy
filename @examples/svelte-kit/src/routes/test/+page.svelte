bf20d1d038055d7e71dfa8d4e3ce7913d00a1b3f
lttb <kenzhaev.artur@gmail.com>
2023-03-18 13:10:57 +0000

    feat(examples/svelte-kit): add test route

---
+++ @examples/svelte-kit/src/routes/test/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import {css} from 'taddy'
+
+    const styles = {h1: css({color: 'orange', textDecoration: 'underline'})}
+</script>
+
+<h1 {...styles.h1}>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
