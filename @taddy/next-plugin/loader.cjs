7b6963aec98c9d369b6d2cc64078b9102e14b797
lttb <kenzhaev.artur@gmail.com>
2023-04-05 00:02:25 +0100

    feat(taddy/next-plugin): initial implementation

---
+++ @taddy/next-plugin/loader.cjs
@@ -0,0 +1,55 @@
+const babel = require('@babel/core');
+const taddyBabelPlugin = require('@taddy/babel-plugin');
+const path = require('path');
+
+/** @type {import('webpack').LoaderDefinition} */
+const loader = function (code) {
+    const callback = this.async();
+    const id = this.resourcePath;
+
+    if (id.includes('.taddy.js')) {
+        callback(null, code);
+
+        return;
+    }
+
+    const root = process.cwd();
+
+    const extname = path.extname(id);
+
+    const isTypescript = extname === '.tsx' || extname === '.ts';
+
+    babel
+        .transformAsync(code, {
+            babelrc: false,
+            configFile: false,
+            ast: false,
+            root,
+            filename: id,
+            parserOpts: {
+                allowAwaitOutsideFunction: true,
+                plugins: [
+                    'importMeta',
+                    'topLevelAwait',
+                    'classProperties',
+                    'classPrivateProperties',
+                    'classPrivateMethods',
+                    'jsx',
+                ].concat(isTypescript ? ['typescript'] : []),
+            },
+            generatorOpts: {
+                decoratorsBeforeExport: true,
+            },
+            plugins: [taddyBabelPlugin],
+            sourceMaps: true,
+            inputSourceMap: false,
+        })
+        .then((result) => {
+            callback(null, result.code, result.map);
+        })
+        .catch((err) => {
+            callback(err);
+        });
+};
+
+module.exports = loader;
