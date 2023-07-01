9f36dc520a3f56363cb29f6833ad37f41d2d341d
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2023-07-01 11:37:52 +0100

    feat(babel-plugin): improve static styles concatenation

--- @taddy/babel-plugin/src/helpers/optimizeStaticStyles.ts
+++ @taddy/babel-plugin/src/helpers/optimizeStaticStyles.ts
@@ -29,6 +29,7 @@ export function optimizeStaticStyles(path: NodePath<t.ObjectExpression>) {
         if (key === 'className' || key === 'style' || key === VARS_KEY) {
             if (currQuasi) {
                 quasis.push(t.templateElement({raw: currQuasi}));
+
                 props.push(
                     t.objectProperty(
                         t.templateLiteral(quasis, expressions),
@@ -38,33 +39,31 @@ export function optimizeStaticStyles(path: NodePath<t.ObjectExpression>) {
             }
 
             props.push(propPath.node);
+
             quasis = [];
             expressions = [];
             currQuasi = '';
+
             continue;
         }
 
-        // if (key === 'className') {
-        //     quasis.push(valuePath.node);
-        //     continue;
-        // }
+        // append " " delimiter for the continuous value or if that's not the first quasi
+        if (currQuasi || quasis.length !== 0) {
+            currQuasi += ' ';
+        }
 
         if (valuePath.isStringLiteral()) {
-            currQuasi += ' ' + key + valuePath.node.value;
-            continue;
+            currQuasi += key + valuePath.node.value;
+        } else if (valuePath.isBooleanLiteral()) {
+            currQuasi += key;
+        } else {
+            currQuasi += key;
+
+            quasis.push(t.templateElement({raw: currQuasi}));
+            expressions.push(valuePath.node as t.Expression);
+
+            currQuasi = '';
         }
-
-        if (valuePath.isBooleanLiteral()) {
-            currQuasi += ' ' + key;
-            continue;
-        }
-
-        currQuasi += ' ' + key;
-
-        quasis.push(t.templateElement({raw: currQuasi}));
-        expressions.push(valuePath.node as t.Expression);
-
-        currQuasi = '';
     }
 
     if (currQuasi) {
@@ -75,13 +74,13 @@ export function optimizeStaticStyles(path: NodePath<t.ObjectExpression>) {
         quasis.push(t.templateElement({raw: ''}));
     }
 
-    if (props.length === 0) {
-        path.replaceWith(t.templateLiteral(quasis, expressions));
-
-        return;
-    }
-
     if (quasis.length) {
+        if (props.length === 0) {
+            path.replaceWith(t.templateLiteral(quasis, expressions));
+
+            return;
+        }
+
         props.push(
             t.objectProperty(
                 t.templateLiteral(quasis, expressions),
