eb6e2620ab21cd868b9bce1f41bd4a8e52f2e6f2
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2024-05-25 19:40:18 +0100

    feat(vite): update build

--- @taddy/vite-plugin/rollup.config.js
+++ @taddy/vite-plugin/rollup.config.js
@@ -3,7 +3,7 @@ import commonConfig from '../../rollup.config.common';
 const config = {
     ...commonConfig,
 
-    input: ['index.js', 'loader.cjs'],
+    input: ['index.js'],
 };
 
 export default config;
