2dfa487e3838a258a98675b1abcd61bb6c92a252
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2024-05-25 19:47:19 +0100

    feat: update package.json

--- taddy/rollup.config.js
+++ taddy/rollup.config.js
@@ -3,7 +3,7 @@ import commonConfig from '../rollup.config.common';
 const config = {
     ...commonConfig,
 
-    input: ['index.ts', 'index.native.ts', 'vue/index.ts'],
+    input: ['index.ts', 'index.native.ts', 'vue/index.ts', 'css.js'],
 };
 
 export default config;
